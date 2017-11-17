53acd4068506f8487d0b7313eace77fc0e4ef789
fisker <lionkay@gmail.com>
2017-11-17 14:32:24 +0800

    remove assign

--- index.js
+++ index.js
@@ -6,9 +6,8 @@
 'use strict'
 
 var prettier = require('prettier')
-var log = (global.fis && fis.log) || console
+var log = global.fis.log
 var assign = Object.assign || global.fis.util.assign
-
 var rcConfig = prettier.resolveConfig.sync('prettier')
 
 module.exports = function(content, file, conf) {
